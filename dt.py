723c54f130f6823ddaec13aa0bd5f471d99c46e2
John Meanor <jmeanor@gmail.com>
2020-02-02 16:26:47 -0500

    Refactor for plots continued.

--- dt.py
+++ dt.py
@@ -77,53 +77,24 @@ def runDT(X_train, X_test, y_train, y_test, data, path):
     complex_test_scores_mean = np.mean(complex_valid_scores, axis=1)
     complex_test_scores_std = np.std(complex_valid_scores, axis=1)
     # log.info(complex_train_scores_mean.shape, complex_train_scores_std.shape, complex_test_scores_mean.shape, complex_test_scores_std.shape)
+    
+    # Create subplots
     _, axes = plt.subplots(1, 2, figsize=(12, 5))
 
     # Plot learning curve
-    learn_title = 'DT - Learning Curve (max_depth: %i) ' % bestParams['max_depth']
+    learn_title = 'DT - Learning Curve (max_depth: %s) ' % bestParams['max_depth']
     learn_vals = (train_sizes, train_scores_mean, train_scores_std, test_scores_mean, test_scores_std)
     labels = ("Training score", "Cross-validation score", "# of samples", "Score")
     graph.plotLearningCurve(axes[0], learn_title, *learn_vals, *labels)
    
-    # axes[0].grid()
-    # axes[0].set_title(learn_title)
-    # axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
-    #                      train_scores_mean + train_scores_std, alpha=0.1,
-    #                      color="r")
-    # axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
-    #                      test_scores_mean + test_scores_std, alpha=0.1,
-    #                      color="g")
-    # axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
-    #              label="Training score")
-    # axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
-    #              label="Cross-validation score")
-    # axes[0].set_ylim((0, 1.1))
-    # axes[0].set_xlabel("# of samples")
-    # axes[0].set_ylabel("Score")
-    # axes[0].legend(loc="best")
-
     # Plot Model-Complexity Curve
-    axes[1].grid()
+    complex_title = 'DT - Complexity Curve (max_depth: %s, ccp_alpha: %d) ' % (bestParams['max_depth'], bestParams['ccp_alpha'])
     maxDepth = np.array(HyperParams['max_depth'], dtype=float)
     maxDepth[0] = np.inf
-
-    axes[1].set_title('DT - Complexity Curve (max_depth: %s, ccp_alpha: %d) ' %
-                      (bestParams['max_depth'], bestParams['ccp_alpha']))
-
-    axes[1].fill_between(maxDepth, complex_train_scores_mean - complex_train_scores_std,
-                         complex_train_scores_mean + complex_train_scores_std, alpha=0.1,
-                         color="b")
-    axes[1].fill_between(maxDepth, complex_test_scores_mean - complex_test_scores_std,
-                         complex_test_scores_mean + complex_test_scores_std, alpha=0.1,
-                         color="m")
-    axes[1].plot(maxDepth, complex_train_scores_mean, 'o-', color="b",
-                 label="Training score")
-    axes[1].plot(maxDepth, complex_test_scores_mean, 'o-', color="m",
-                 label="Cross-validation score")
-    axes[1].set_ylim((0, 1.1))
-    axes[1].set_xlabel("Maximum Depth Param")
-    axes[1].set_ylabel("Score")
-    axes[1].legend(loc="best")
+    X = maxDepth
+    complex_vals = (X, complex_train_scores_mean, complex_train_scores_std, complex_test_scores_mean, complex_test_scores_std)
+    labels = ("Training score", "Cross-validation score", "Maximum Depth Param", "Score")
+    graph.plotComplexityCurve(axes[1], complex_title, *complex_vals, *labels)
 
     saveDir = os.path.join(path, 'DT - max_depth.png')
     plt.savefig(saveDir, bbox_inches='tight')
--- graph.py
+++ graph.py
@@ -6,9 +6,6 @@ matplotlib.use("macOSX")
 # Plot learning curve
 def plotLearningCurve(axes, title, train_sizes, train_scores_mean, train_scores_std, test_scores_mean, 
     test_scores_std, label1="", label2="", xlabel="", ylabel=""):
-    # _, axis = plt.subplots(1, 1)
-    # axis = plt.plot()
-    # axis = matplotlib.axes
     axes.grid()
     axes.set_title(title)
     axes.fill_between(train_sizes, train_scores_mean - train_scores_std,
@@ -21,6 +18,25 @@ def plotLearningCurve(axes, title, train_sizes, train_scores_mean, train_scores_
                  label=label1)
     axes.plot(train_sizes, test_scores_mean, 'o-', color="g",
                  label=label2)
+    axes.set_ylim((0, 1.1))
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.legend(loc="best")
+
+# Plot learning curve
+def plotComplexityCurve(axes, title, X, train_scores_mean, train_scores_std, test_scores_mean, 
+    test_scores_std, label1="", label2="", xlabel="", ylabel=""):
+    axes.grid()
+    axes.set_title(title)
+
+    axes.fill_between(X, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="b")
+    axes.fill_between(X, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="m")
+    axes.plot(X, train_scores_mean, 'o-', color="b", label=label1)
+    axes.plot(X, test_scores_mean, 'o-', color="m", label=label2)
     axes.set_ylim((0, 1.1))
     axes.set_xlabel(xlabel)
     axes.set_ylabel(ylabel)
